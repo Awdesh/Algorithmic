8e771a0670445d46c4ea0d00c5a8481be0d6bb89
Awdesh Sharma <awdesh.sharma1@gmail.com>
2016-09-12 21:44:25 -0700

    Update Program.cs

--- Tree/Program.cs
+++ Tree/Program.cs
@@ -18,11 +18,6 @@ namespace Tree
             this.data = data;
         }
 
-        public TreeNode()
-        {
-
-        }
-
         public TreeNode getLeft()
         {
             return left;
